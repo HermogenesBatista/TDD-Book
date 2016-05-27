c32fc67a5bedbaa2e3bce81a3beb1c374bf66bae
HermogenesBatista <exemogenes@hotmail.com>
2016-05-26 23:49:31 -0300

    change url redirect and modified settings to connect with mysql

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -80,8 +82,12 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tdd_book',
+        'USER': os.environ['USERNAME_DB'],
+        'PASSWORD': os.environ['PASSWORD_DB'],
+        'HOST': os.environ.get('HOST_DB', 'localhost'),
+        'PORT': os.environ.get('HOST_DB', ''), #empty for default port
     }
 }
 
